5cd649113f3ca3488002e1771a757bc8620e0044
achmed <achmedzhanov@gmail.com>
2016-04-22 16:23:15 +0400

    убрал лишний код

--- app/scripts/PolygonAlgs.js
+++ app/scripts/PolygonAlgs.js
@@ -3,47 +3,6 @@ var PolygonAlgs = {};
 (function(PolygonAlgs, PolyK, rbush, ClipperLib) {
     "use strict";
 
-    function coordinatesToArray(coordinates) {
-        //пропускаем последнюю точку если она совпадает с первой
-        let popLastCoordinates = coordinates.length > 1 && (coordinates[0][0] == coordinates[coordinates.length - 1][0] && coordinates[0][1] == coordinates[coordinates.length - 1][1])
-        let ret = new Array((coordinates.length - (popLastCoordinates ? 1 : 0)) * 2);
-        // некогжа выснять порядок вершин - по часовой стрелке или притив часвой
-        // поэтому допускаем вольность и ...
-        // предполагаем что вершины полигоа расположены по часовой стрелке
-        coordinates = coordinates.slice(0).reverse();
-
-        let i = ret.length / 2;
-        while (i--) {
-            ret[i * 2] = coordinates[i][0];
-            ret[i * 2 + 1] = coordinates[i][1];
-        }
-        return ret;
-    }
-
-    function getBoundingBox(coordinates) {
-        let left = coordinates[0][0];
-        let right = left;
-        let top = coordinates[0][1];
-        let bottom = top;
-        let i = coordinates.length;
-        while (i-- > 0) {
-            left = Math.min(left, coordinates[i][0]);
-            right = Math.max(right, coordinates[i][0]);
-            bottom = Math.min(bottom, coordinates[i][1]);
-            top = Math.max(top, coordinates[i][1]);
-        }
-
-        return {
-            left: left,
-            right: right,
-            top: top,
-            bottom: bottom
-        };
-    }
-
-    function isCoveredBox(candidateBox, destinationBox) {
-        return candidateBox.left >= destinationBox.left && candidateBox.right <= destinationBox.right && candidateBox.bottom >= destinationBox.bottom && candidateBox.top <= destinationBox.top;
-    }
 
     function getBoundingBoxArray(coordinates) {
         let left = coordinates[0][0];
@@ -59,59 +18,6 @@ var PolygonAlgs = {};
         }
 
         return [left*clipperFactor, bottom*clipperFactor, right*clipperFactor, top*clipperFactor];
-    }
-
-    function getBoundingBoxArrayFromClipperPath(coordinates) {
-      let left = coordinates[0].X;
-      let right = left;
-      let top = coordinates[0].Y;
-      let bottom = top;
-      let i = coordinates.length;
-      while (i-- > 0) {
-          left = Math.min(left, coordinates[i].X);
-          right = Math.max(right, coordinates[i].X);
-          bottom = Math.min(bottom, coordinates[i].Y);
-          top = Math.max(top, coordinates[i].Y);
-      }
-
-      return [left, bottom, right, top];
-    }
-
-    function isCoveredBoxArray(candidateBox, destinationBox) {
-        return candidateBox[0] >= destinationBox[0] && candidateBox[2] <= destinationBox[2] && candidateBox[3] <= destinationBox[3] && candidateBox[1] >= destinationBox[1];
-    }
-
-    function isPointInRect(x, y, a) {
-        return x >= a[0] && y >= a[1] && x <= a[2] && y <= a[3];
-    }
-
-    function isEqualPaths(paths1, paths2) {
-      if(paths1.length != paths2.length) {
-          return false;
-      }
-      //if(ClipperLib.Clipper.Area())
-      let i = paths1.length;
-      while(i--) {
-        if(paths1[i].length != paths2[i].length) {
-            return false;
-        }
-        let j = paths1[i].length;
-        while(j--) {
-          let k = paths1[i].length;
-          let found = false;
-          while(k--) {
-            if(paths1[i][j].X == paths2[i][k].X
-            && paths1[i][j].Y == paths2[i][k].Y) {
-                found = true;
-                break;
-            }
-          }
-          if(!found) {
-            return false;
-          }
-        }
-      }
-      return true;
     }
 
     // множитель задает точность вычислений перекрытий полигонов
@@ -204,14 +110,6 @@ var PolygonAlgs = {};
         return filtered.map(v => v.feature);
     }
 
-    function pathToGeo(path) {
-      return path.map((v) => {
-        if(Array.isArray(v))
-          return pathToGeo(v);
-        return [v.X,v.Y];
-      });
-    }
-
     function toClipperPath(coordinates) {
         let result = new Array((coordinates[0][0] == coordinates[coordinates.length - 1][0] && coordinates[0][1] == coordinates[coordinates.length - 1][1]) ? coordinates.length - 1 : coordinates.length);
         let i = result.length;
@@ -230,53 +128,6 @@ var PolygonAlgs = {};
         return result;
     }
 
-
-    function _isCovered(candidate, destination) {
-        //console.log('_isCovered');
-        let cPoints = candidate.feature.geometry.coordinates[0];
-
-        // быстрая проверка по включению обрамляющего прямоугольника
-        // if(!isCoveredBox(candidate.boundingBox, destination.boundingBox)) {
-        //   return false;
-        // }
-
-        //кэшируем массив координат
-        if (destination.cArray == undefined) {
-            destination.cArray = coordinatesToArray(destination.feature.geometry.coordinates[0]);
-        }
-        //кэшируем признак выпуклости
-        if (destination.isConvex == undefined) {
-            destination.isConvex = PolyK.IsConvex(destination.cArray) || PolyK.IsConvex(coordinatesToArray(destination.feature.geometry.coordinates[0].slice(0).reverse()));
-        }
-
-        let covered = true;
-        for (let i = 0; i < cPoints.length; i++) {
-            // если точка не попадает в проверяемый полигон то false
-            if (!PolyK.ContainsPoint(destination.cArray, cPoints[i][0], cPoints[i][1])) {
-                return false;
-            }
-        }
-        // если проверяемый полигон выпуклый, то достаточно проверить только вхождения точек
-        if (destination.isConvex) {
-            return true;
-        }
-        // иначе проверяем пересечение всех ребер
-        console.log('Проверка ребер не реализована');
-
-        return false;
-    }
-
-    function isCovered(candidate, destination) {
-        return _isCovered({
-            feature: candidate,
-            boundingBox: getBoundingBox(candidate.geometry.coordinates[0])
-        }, {
-            feature: destination,
-            boundingBox: getBoundingBox(destination.geometry.coordinates[0])
-        });
-    }
-
-    PolygonAlgs.isCovered = isCovered;
     PolygonAlgs.filterCoveredPolygons = filterCoveredPolygons;
 
 })(PolygonAlgs, PolyK, rbush, ClipperLib);
--- test/spec/test.js
+++ test/spec/test.js
@@ -2,60 +2,6 @@
   'use strict';
 
   describe('PolygonAlgs', function () {
-    describe('#isCovered', function () {
-      it('Должен вернуть true когда один полигон накрывает целиком (гео коорлдинаты)', function () {
-        var p  = {"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[-56.736721,79.502541],[-76.597969,81.201027],[-74.45649,82.200592],[-53.047604,80.343918],[-56.736721,79.502541]]]}};
-        var d = {"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[-55.736721,78.502541],[-76.597969,81.201027],[-74.45649,82.200592],[-53.047604,80.343918],[-55.736721,78.502541]]]}};
-        var result = PolygonAlgs.isCovered(p, d);
-        assert.isTrue(result);
-      });
-
-      it('Должен вернуть true когда один полигон накрывает целиком (гео коорлдинаты)', function () {
-        var p  = {"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[-56.736721,79.502541],[-76.597969,81.201027],[-74.45649,82.200592],[-53.047604,80.343918],[-56.736721,79.502541]]]}};
-        var d = {"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[-56.736721,79.502541],[-76.597969,81.201027],[-74.45649,82.200592],[-53.047604,80.343918],[-56.736721,79.502541]]]}};
-        var result = PolygonAlgs.isCovered(p, d);
-        assert.isTrue(result);
-      });
-
-      it('Должен вернуть true когда один полигон накрывает целиком (целые координаты)', function () {
-        var p  = {"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[1,1],[1,4],[4,4],[4,1],[1,1]]]}};
-        var d = {"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[0,0],[0,5],[5,5],[5,0],[0,0]]]}};;
-        var result = PolygonAlgs.isCovered(p, d);
-        assert.isTrue(result);
-      });
-
-      it('Должен вернуть true когда полигоны совпадают (целые коорлдинаты)', function () {
-        var p = {"type":"Feature","geometry":{"type":"Polygon","coordinates":[([[0,0],[0,5],[5,5],[5,0],[0,0]])]}};;
-        var d = {"type":"Feature","geometry":{"type":"Polygon","coordinates":[([[0,0],[0,5],[5,5],[5,0],[0,0]])]}};
-        var result = PolygonAlgs.isCovered(p, d);
-        assert.isTrue(result);
-      });
-
-
-
-      it('Должен вернуть true когда один полигон накрывает целиком и ребер ориентированы по часовой стрелке (целые координаты)', function () {
-        var p  = {"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[1,1],[4,1],[4,4],[1,4],[1,1]]]}};
-        var d = {"type":"Feature","geometry":{"type":"Polygon","coordinates":[([[0,0],[0,5],[5,5],[5,0],[0,0]])]}};;
-        var result = PolygonAlgs.isCovered(p, d);
-        assert.isTrue(result);
-      });
-
-    });
-/*
-{
-"type":"FeatureCollection",
-"features":[
-{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[-56.736721,79.502541],[-76.597969,81.201027],[-74.45649,82.200592],[-53.047604,80.343918],[-56.736721,79.502541]]]}},
-{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[-55.736721,78.502541],[-76.597969,81.201027],[-74.45649,82.200592],[-53.047604,80.343918],[-55.736721,78.502541]]]}},
-{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[47.954506,80.722702],[70.071335,82.686768],[77.297005,78.996407],[60.34584,77.564949],[47.954506,80.722702]]]}},
-{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[-96.827904,19.897907],[-99.194847,20.321608],[-98.84391,22.068865],[-96.44857,21.648794],[-96.827904,19.897907]]]}},
-{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[94.583733,-62.176575],[101.554695,-60.51865],[105.339142,-63.530411],[97.796455,-65.356491],[94.583733,-62.176575]]]}}
-]
-}
-
-
-*/
-
       describe('#filterCoveredPolygons', function () {
         it('Должен отфильтровать полигон, который накрывается целиком одним полигоном (гео координаты)', function () {
           var small  = {"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[-56.736721,79.502541],[-76.597969,81.201027],[-74.45649,82.200592],[-53.047604,80.343918],[-56.736721,79.502541]]]}};
